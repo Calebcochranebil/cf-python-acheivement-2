a32e23c5365f1eb71f095203eac641e243c8553c
calebcochranebil <calebcochranebil33@gmail.com>
2023-10-25 11:58:00 -0700

    versionupdate

--- A2_Recipe_App/src/recipeingredients/admin.py
+++ A2_Recipe_App/src/recipeingredients/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import RecipeIngredient
 
 # Register your models here.
-admin.site.register(Recipe, admin.ModelAdmin)
+admin.site.register(RecipeIngredient)
--- A2_Recipe_App/src/recipeingredients/apps.py
+++ A2_Recipe_App/src/recipeingredients/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class RecipeingredientsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recipeingredients'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "recipeingredients"
